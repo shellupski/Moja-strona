610712e26221ba5e3a6b0d55e1b0ad0bf57ef5b3
rastuszek <skorupa7@o2.pl>
2025-09-29 16:39:33 +0200

    update deploy1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router basename="/Moja-strona">
+        <Router basename="/Crusty-IT">
             <Navbar />
 
             {/* Dodajemy główny kontener na treść strony */}
